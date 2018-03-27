39eba71c62ce48fd9a62cf3174f76cb99c387179
kexinhu <kexinhu@tencent.com>
2018-03-27 23:55:56 +0800

    add tower walk

--- cpp/binary_tree.cc
+++ cpp/binary_tree.cc
@@ -1,8 +1,9 @@
 // g++ -std=c++11 -o tree binary_tree.cc &&./tree.exe
 
-#include<iostream>
 #include<algorithm>
+#include<iostream>
 #include<iterator>
+#include<stack>
 #include<string.h>
 using namespace std;
 
@@ -12,6 +13,7 @@ struct tree{
     struct tree* right;
 };
 
+// 从先序序列（含有#）中建立二叉树
 tree* create_tree(char *& p_str ){
     if(*p_str=='#'||*p_str=='\0') {
         return NULL;
@@ -19,8 +21,8 @@ tree* create_tree(char *& p_str ){
     else{
         //tree* root = (tree*)malloc(sizeof(tree));
         tree* root = new tree();
-        root->data=*(p_str++);
-        root->left=create_tree(p_str);
+        root->data=*(p_str);
+        root->left=create_tree(++p_str);
         root->right=create_tree(++p_str); // 右子树时，指针必须要再次移动
         return root;
     }
@@ -32,13 +34,17 @@ void visit(tree* root){
     cout<<root->data<<" ";
 }
 
+void swap_child(tree* root){
+    std::swap(root->left,root->right);
+}
+
 void delete_tree(tree* root){
     cout<<"delete:"<<root->data<<"\n";
     delete root;
 }
 
 // 前序遍历
-void pre_order_visit(tree* root,void(*fun)(tree*)){
+void pre_order_visit(tree* root,void(*fun)(tree*)){ // 函数指针
     if(root==NULL) return;
     fun(root);
     pre_order_visit(root->left,fun);
@@ -53,6 +59,26 @@ void in_order_visit(tree* root,void(*fun)(tree*)){
     in_order_visit(root->right,fun);
 }
 
+// 中序非递归遍历
+void in_order_non_recursive(tree *root){
+    if(root == NULL) return;
+    stack<tree*> st;
+    tree*p=root;
+    while(p||!st.empty()){
+        if(p) { // 只会压入非空指针到栈内
+            st.push(p);
+            p = p->left;// 先将左边的全部入栈
+        }
+        else{
+            p=st.top(); //top会返回元素
+            st.pop(); // 注意:c++ pop时不会返回元素
+            visit(p); // 元素出栈时立即访问
+            p = p->right;
+        }
+    }
+}
+
+
 // 后序遍历
 void post_order_visit(tree* root,void(*fun)(tree*)){
     if(root==NULL) return;
@@ -60,6 +86,29 @@ void post_order_visit(tree* root,void(*fun)(tree*)){
     post_order_visit(root->right,fun);
     fun(root);
 }
+
+// 二叉树的镜像
+void post_mirror(tree* root){
+    if(root==NULL) return;
+    post_mirror(root->left);
+    post_mirror(root->right);
+    std::swap(root->left,root->right);
+}
+
+void pre_mirror(tree* root){
+    if(root==NULL) return;
+    std::swap(root->left,root->right);
+    pre_mirror(root->left);
+    pre_mirror(root->right);
+}
+
+int depth(tree* root){
+    if(root==NULL) return 0;
+    int left_depth=depth(root->left);
+    int right_depth = depth(root->right);
+    return std::max(left_depth,right_depth)+1;
+}
+
 
 int main(){
     // 利用完全二叉树的数组表示，#代表空结点
@@ -81,15 +130,56 @@ int main(){
     tree* root=create_tree(p_str);
 
     cout<<"pre order:"<<endl;
-    pre_order_visit(root,visit);
+    pre_order_visit(root,visit);// a b c d e g f
     cout<<"\nin order:"<<endl;
-    in_order_visit(root,visit);
+    in_order_visit(root,visit); // c b e g d f a
     cout<<"\npost order:"<<endl;
+    post_order_visit(root,visit);// c g e f d b a
+
+    // 二叉树镜像
+    /*
+                a
+                 \
+                  b
+                 / \
+                d   c
+               / \
+              f   e
+                   \
+                    g
+    */
+    // 镜像
+    post_mirror(root);
+    cout<<"\npre order after post mirror:"<<endl;
+    pre_order_visit(root,visit);
+    // 再镜像回来
+    post_mirror(root);
+    cout<<"\npre order after two mirror:"<<endl;
+    pre_order_visit(root,visit);
+
+    cout<<"\npre order after pre mirror:"<<endl;
+    pre_mirror(root);
+    pre_order_visit(root,visit);
+    cout<<"\npre order after two mirror:"<<endl;
+    pre_mirror(root);
+    pre_order_visit(root,visit);
+    cout<<"\npost order :"<<endl;
     post_order_visit(root,visit);
 
+    cout<<"\npost mirror by visit:"<<endl;
+    pre_order_visit(root,swap_child);
+    pre_order_visit(root,visit);
+    cout<<"\npre two mirror by visit:"<<endl;
+    pre_order_visit(root,[](tree* root){ std::swap(root->left,root->right);}); // 匿名函数，不要捕获外部变量
+    pre_order_visit(root,visit);
+    //----树的深度
+    cout<<"\ntree depth:"<<depth(root)<<endl;
+    cout<<"\nin order non recursive visit:"<<endl;
+    in_order_non_recursive(root);
+    cout<<"\nin odrer recursive visit:"<<endl;
+    in_order_visit(root,visit);
+    //--------------------------
     cout<<"\ndelete tree:"<<endl;
     post_order_visit(root,delete_tree);
-    //--------------------
-    //delete root;
     return 0;
 }
---
+++ cpp/tower_walk.cc
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+/************************************************************************/
+/* 数塔问题                                                               */
+/************************************************************************/
+const int N = 50;//为了算法写起来简单，这里定义一个足够大的数用来存储数据(为了避免运算过程中动态申请空间，这样的话算法看起来比较麻烦，这里只是为了算法看起来简单)
+int data[N][N]={
+    {9},
+    {12,15},
+    {10,6,8},
+    {2,18,9,5},
+    {19,7,10,4,16}
+};//存储数塔原始数据
+int n=5;//塔的层数
+int tower_sum[N][N];//存储动态规划过程自底向上的求和
+
+/*动态规划实现数塔求解*/
+void tower_walk()
+{
+    // tower_sum初始化
+    for (int i = 0; i < n; ++i)
+    {
+        tower_sum[n - 1][i] = data[n - 1][i]; // 最底层的直接赋值
+    }
+
+    int temp_max;
+    for (int i = n - 1; i >= 0; --i) // i 指层数
+    {
+        for (int j = 0; j <= i; ++j)
+        {
+            // 使用递推公式计算tower_sum的值
+            temp_max = std::max(tower_sum[i + 1][j], tower_sum[i + 1][j + 1]);
+            tower_sum[i][j] = temp_max + data[i][j]; // 更新当前层的tower_sum
+        }
+    }
+}
+
+/*打印最终结果*/
+void print_result()
+{
+    cout << "最大路径和：" << tower_sum[0][0] << '\n';
+    int node_value;
+    // 首先输出塔顶元素
+    cout << "最大路径：" << data[0][0];
+    int j = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        node_value = tower_sum[i - 1][j] - data[i - 1][j];
+        /* 如果node_value == tower_sum[i][j]则说明下一步应该是data[i][j]；如果node_value == tower_sum[i][j + 1]则说明下一步应该是data[i][j + 1]*/
+        if (node_value == tower_sum[i][j + 1]) ++j;
+        cout << "->" << data[i][j];
+    }
+    cout << endl;
+}
+
+int main()
+{
+    /*
+    cout << "输入塔的层数：";
+    cin >> n;
+    cout << "输入塔的节点数据(第i层有i个节点)：\n";
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = 0; j <= i; ++j)
+        {
+            cin >> data[i][j];
+        }
+    }
+    */
+
+    tower_walk();
+    print_result();
+}
