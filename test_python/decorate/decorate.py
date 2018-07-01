7cde7df179fbe8059579ed9d691f535bf91be87d
hkxIron <hukexin0000@126.com>
2018-07-01 22:34:54 +0800

    update

--- test_python/decorate/decorate.py
+++ test_python/decorate/decorate.py
@@ -17,3 +17,25 @@ for i in range(2):
     print("i:%d"%i)
     sleep(1)
     foo()
+
+
+# 用装饰器来实现对类成员的约束
+# decorator with arguments
+def hkx_deco(*args, **kwargs):
+    def wrapper(cls):
+        for k, v in kwargs.items():
+            print('key: %s, value: %s' % (k, v))
+        return cls
+    return wrapper
+
+# 先会去执行decorate
+@hkx_deco(name=str, age=int, gender=str, salary=float)  # People = deco(People)
+class People(object):
+    def __init__(self, name, age, gender, salary):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.salary = salary
+
+p1 = People('J', 18, 'male', 9.9)
+p2 = People(324, 18, 'male', 9.9)
