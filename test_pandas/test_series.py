465edd4eb5965a7f5cb8dacf75a64140f0d3440c
hkxIron <hukexin0000@126.com>
2018-08-26 16:01:04 +0800

    updat

---
+++ test_pandas/test_series.py
@@ -0,0 +1,16 @@
+from pandas import Series, DataFrame
+import pandas as pd
+from numpy.random import randn
+import numpy as np
+import os
+np.random.seed(12345)
+obj = Series([4, 7, -5, 3])
+print("obj:", obj)
+print("obj.values:", obj.values)
+print("obj.index:", obj.index)
+
+#-------------
+obj2 = Series([4, 7, -5, 3], index=['d', 'b', 'a', 'c'])
+partialRows = obj2[['c', 'a', 'd']] # 选取Series中的部分行
+print("partial rows:\n", partialRows)
+
