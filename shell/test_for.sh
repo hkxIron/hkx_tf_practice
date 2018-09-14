c61f5e7424afc5cbcce0c8ca5cfd1a1e90174345
hukexin <hukexin@xiaomi.com>
2018-09-14 22:22:44 +0800

    udpate

--- shell/test_for.sh
+++ shell/test_for.sh
@@ -5,6 +5,13 @@ do
     echo "print: $x"
 done
 IFS="$OLD_IFS"
+OLD_IFS=$IFS
+IFS=" "
+for x in $(echo "one two three")
+do
+    echo "print: $x"
+done
+IFS="$OLD_IFS"
 
 echo "或者使用数组"
 TABLES=("houyi_rules" "domain_dicts") # shell数组
