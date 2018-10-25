f279c33cd6ff65049e1b349697217477bb81ad56
hkxIron <hkxIron@users.noreply.github.com>
2018-10-25 17:59:19 +0800

    Update mcmc_mh.py

--- stats/mcmc/mcmc_mh.py
+++ stats/mcmc/mcmc_mh.py
@@ -9,6 +9,15 @@ import matplotlib.pyplot as plt
 
 M-H采样是Metropolis-Hastings采样的简称，这个算法首先由Metropolis提出，被Hastings改进，因此被称之为Metropolis-Hastings采样或M-H采样
 M-H采样解决了我们上一节MCMC采样接受率过低的问题。
+
+本例中，要模拟的是正态分布的样本
+在例子里，我们的目标平稳分布是一个均值3，标准差2的正态分布，
+
+而选择的马尔可夫链状态转移矩阵Q(i,j)的条件转移概率是以i为均值,方差1的正态分布在位置j的值。
+
+这个例子仅仅用来让大家加深对M-H采样过程的理解。
+毕竟一个普通的一维正态分布用不着去用M-H采样来获得样本。
+
 """
 def metropolis_hastings_sampling():
     # 计算theta处的概率密度
@@ -24,7 +33,8 @@ def metropolis_hastings_sampling():
         t = t + 1
         # 1. 从条件概率分布Q(x|xt)中采样得到样本x∗
         # pi_star为只有一个元素的列表,从正态分布中选出一个元素X
-        pi_star = norm.rvs(loc= pi[t - 1], scale=sigma, size=1, random_state=None) # 从条件分布Q(x|x_t)中采样,可以看出 均值有变化, pi_star只有一个元素
+        # 转移矩阵Q(i,j)的条件转移概率是以i为均值,方差1的正态分布在位置j的值 (i->j)
+        pi_star = norm.rvs(loc= pi[t - 1], scale=sigma, size=1, random_state=None) # 从条件分布Q(x|x_t)中采样,可以看出均值有变化, pi_star只有一个元素
         # 2. 从均匀分布采样u∼uniform[0,1]
         u = random.uniform(0, 1)
         # pi[t-1]为上一时刻的采样元素
