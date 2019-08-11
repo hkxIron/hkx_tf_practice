650aed173891f9304322cfbb27298133e24b5429
hkxIron <hukexin0000@126.com>
2019-08-11 23:37:32 +0800

    add info

--- test_numpy/ReinforceLearning/Yahoo_recommendation/policy_disjoint.py
+++ test_numpy/ReinforceLearning/Yahoo_recommendation/policy_disjoint.py
@@ -24,12 +24,12 @@ class DisjointPolicy():
         """
 
         # articles: id -> embedding map
-        n_articles = len(article_id_to_embed_map)
+        n_articles = len(article_id_to_embed_map) # 文章个数为k, 即k个arm
         self.article_id_to_index = {}
         self.Aa = np.zeros((n_articles, d, d)) # [k, d, d], k个arm, 每个arm d维
         self.Aa_inv = np.zeros((n_articles, d, d)) # [k, d, d], k个arm, 每个arm d维
-        self.ba = np.zeros((n_articles, d, 1)) # [k, d, 1]
-        self.theta = np.zeros((n_articles, d, 1)) # [k, d, 1] , Aa*theta = ba
+        self.ba = np.zeros((n_articles, d, 1)) # [k, d, 1], k个arm,每个arm d维的pay-off向量
+        self.theta = np.zeros((n_articles, d, 1)) # [k, d, 1] , Aa*theta = ba, k个arm,每个arm 有d维的参数theta
         i = 0
         for key in article_id_to_embed_map:
             self.article_id_to_index[key] = i
@@ -50,12 +50,14 @@ class DisjointPolicy():
             """
             用reward更新选中arm的参数
             """
-            # x:[d, 1]
-            self.Aa[self.max_arm_index] += np.outer(self.x, self.x)
-            self.Aa_inv[self.max_arm_index] = np.linalg.inv(self.Aa[self.max_arm_index])
+            # x:[d, 1],其中x:为user_feature向量
+            # 论文中算法1:Aa <- Aa + x_(t,a)*x_(t,a)^T
+            self.Aa[self.max_arm_index] += np.outer(self.x, self.x) # Aa:[d, d]
+            self.Aa_inv[self.max_arm_index] = np.linalg.inv(self.Aa[self.max_arm_index]) # Aa_inv:[d, d]
             # 更新arm的收益ba
+            # 论文中算法1:ba <- ba + r_t*x_(t,a)
             self.ba[self.max_arm_index] += r * self.x
-            # 更瓣theta, Aa*theta = ba
+            # 更新theta,论文中算法1: Aa*theta = ba  => theta_a <- A_a^(-1)*ba
             self.theta[self.max_arm_index] = self.Aa_inv[self.max_arm_index].dot(self.ba[self.max_arm_index])
         else:
             pass
@@ -65,22 +67,22 @@ class DisjointPolicy():
         #global x
 
         # TODO:注意：在disjoint中只用user有feature,而arm feature并没有用
-
         article_len = len(candidate_articles)
         # user_feature为xt
         self.x = np.array(user_features).reshape((d,1)) # x:[d, 1]
         x_t = np.transpose(self.x) # x_t:[1, d]
         candidate_article_indexs = [self.article_id_to_index[article] for article in candidate_articles] # list
 
-        # 取前k个arm
+        # 取目标arm的前k个arm序列
         # theta: [k, d, 1]
         # x:[d, 1]
         article_thetas = self.theta[candidate_article_indexs] # [k, d, 1]
-        article_thetas_trans = np.transpose(article_thetas, (0, 2 ,1)) # [k, 1, d]
+        article_thetas_trans = np.transpose(article_thetas, (0, 2, 1)) # [k, 1, d]
+        # 论文中算法1:p_{t,a}=theta*x_{t,a} + alpha*sqrt(x_{t,a}^T*A_a*x_{t,a})
         exploitation = np.matmul(article_thetas_trans, self.x) # [k, 1, 1]
         # xt:[1, d], Aa_inv:[k, d, d], x:[d, 1]
         A_inv_x = self.Aa_inv[candidate_article_indexs].dot(self.x) # [k, d, 1]
-        exploration = np.sqrt(np.matmul(x_t, A_inv_x))
+        exploration = np.sqrt(np.matmul(x_t, A_inv_x)) # [k, 1, 1]
 
         # 所有的文章都算一次ucb，而只需要选出收益最大的即可
         reward = exploitation + alpha * exploration
