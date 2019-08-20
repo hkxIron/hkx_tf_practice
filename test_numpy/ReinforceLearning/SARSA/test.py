03a97f01dacd5a9b8e096d834f9b0944d5585c54
hkxIron <hukexin0000@126.com>
2019-08-20 09:05:54 +0800

    add info

---
+++ test_numpy/ReinforceLearning/SARSA/test.py
@@ -0,0 +1,25 @@
+# https://blog.csdn.net/c602273091/article/details/79008755
+import gym
+from gridworld import GridWorldEnv
+from gym import spaces
+
+env = GridWorldEnv(n_width=12,          # 水平方向格子数量
+                   n_height = 4,        # 垂直方向格子数量
+                   u_size = 60,         # 可以根据喜好调整大小
+                   default_reward = -1, # 默认格子的即时奖励值
+                   default_type = 0)    # 默认的格子都是可以进入的
+env.action_space = spaces.Discrete(4)   # 设置行为空间数量
+# 格子世界环境类默认使用0表示左，1：右，2：上，3:下，4,5,6,7为斜向行走
+# 具体可参考_step内的定义
+# 格子世界的观测空间不需要额外设置，会自动根据传输的格子数量计算得到
+env.start = (0,0)
+env.ends = [(11,0)]
+for i in range(10):
+    env.rewards.append((i+1,0,-100))
+    env.ends.append((i+1,0))
+env.types = [(5,1,1),(5,2,1)]
+env.refresh_setting()
+env._reset()
+env._render()
+input("press any key to continue...")
+
