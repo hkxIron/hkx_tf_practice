11427dac6a6f5f7e1501c3424ca93541a7b2b99f
hkxIron <hukexin0000@126.com>
2020-05-19 00:46:04 +0800

    add

--- cpp/connect_first_and_last.cc
+++ cpp/connect_first_and_last.cc
@@ -81,6 +81,63 @@ void print(node* p){
     std::cout<<"\n";
 }
 
+// 将链表逆序: 1 -> 2-> 3-> 4-> 5
+// 1  2,3,4,5
+// 2,1  3,4,5
+// 3,2,1  4,5
+// 5 -> 4 -> 3 -> 2 -> 1
+node* reverse_link_list(node *head) {
+    node* reversedHead = NULL;
+    node* p = head;
+    node* prev = NULL;
+    while(p!=NULL) {
+        node* pNext = p->next;
+        if(pNext==NULL) reversedHead = p;
+        //改变指针
+        p->next = prev;
+        //移动
+        prev=p;
+        p=pNext;
+    }
+    return reversedHead;
+}
+
+// 使用反转法
+// input: 1 -> 2 -> 3 | ->4 ->5 ->6
+// left: 1 -> 2 ->3
+// 右边反转: 6 -> 5 -> 4
+//连接: 1 -> 6 -> 2 -> 5 -> 3 -> 4
+node* connect_head_tail_by_reverse(node* pList) {
+    if(pList==NULL||pList->next == NULL) return pList;
+    // 求取list长度
+    int len = get_len(pList);
+    int last_left_cnt = len/2;
+    if(len&0x1) { // 奇数
+        last_left_cnt = (len+1)/2;
+    }
+    // 找到右边的第一个node, 以减小耗时
+    node* last_left_node = get_k_node(pList, last_left_cnt);
+    node* first_right_node = last_left_node->next;
+    last_left_node->next = NULL; // 断开左边的最后一个结点的next指针
+
+    // 反转右边的链表
+    node* reversed_right_list = reverse_link_list(first_right_node);
+    // 合并两个链表
+    node* pL = pList;
+    node* pR = reversed_right_list;
+    while(pL&&pR) {
+       node* pLNext = pL->next;
+       node* pRNext = pR->next;
+
+       pL->next = pR;
+       pR->next = pLNext;
+
+       pL = pLNext;
+       pR = pRNext;
+    }
+    return pList;
+}
+
 int main(){
     {
         int a[] = {1,2, 3, 4, 5 ,6 };
@@ -101,6 +158,7 @@ int main(){
 
     {
 
+        std::cout<<"------------"<<std::endl;
         int a[] = {1,2, 3, 4, 5};
         int len = sizeof(a)/sizeof(int);
         node* head;
@@ -118,10 +176,33 @@ int main(){
 
     }
 
+    {
+        std::cout<<"------------"<<std::endl;
+        int a[] = {1,2, 3, 4, 5,6};
+        int len = sizeof(a)/sizeof(int);
+        node* head;
+        node* pre = NULL;
+        for(int i=0;i<len;i++){
+            node* p = new node();
+            p->x = a[i];
+            if(pre==NULL) head = p;
+            else pre->next = p;
+            pre=p;
+        }
+        print(head);
+        //----------
+        print(connect_head_tail_by_reverse(head));
+    }
+
     /**
  1 2 3 4 5 6
  1 6 2 5 3 4
+------------
  1 2 3 4 5
  1 5 2 4 3
+------------
+ 1 2 3 4 5 6
+ 1 6 2 5 3 4
+
     **/
 }
